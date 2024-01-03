6f98b002157c7f290b96d3a7200c83092a0603df
Theodora Chu <theodorachu@gmail.com>
2024-01-02 19:58:39 -0800

    project set up

---
+++ src/pages/test.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class DefaultComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello, this is a default React component!
+      </div>
+    );
+  }
+}
+
+export default DefaultComponent;
