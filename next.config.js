4f1524b11c60d35960457b41c5ba9a1f968ae2b2
Theodora Chu <theodorachu@gmail.com>
2024-05-07 15:48:16 -0700

    cleaning project to start chatbot ui, updated config to allow for env variables

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
+require('dotenv').config();
+const webpack = require('webpack');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+//module.exports = nextConfig
+module.exports = {
+  webpack: (config, {isServer}) => {
+    if(isServer) {
+      config.plugins.push(new webpack.EnvironmentPlugin({
+        OAI_KEY: process.env.OAI_KEY,}));
+    }
+    return config;
+  },
+};
---
+++ src/pages/index.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+const Home = () => {
+  const apiKey = process.env.OAI_KEY;
+  const [message, setMessage] = useState("");
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setMessage(event.target.value);
+  }
+
+  const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(message);
+    setMessage("");
+  }
+
+  return (
+    <div>
+      <h1>What's on your mind today?</h1>
+      <form onSubmit={handleFormSubmit}>
+        <input 
+          type="text"
+          placeholder="Type thoughts here"
+          value={message}
+          onChange={handleInputChange}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default Home;
